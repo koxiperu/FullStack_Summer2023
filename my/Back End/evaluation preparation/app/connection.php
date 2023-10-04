0db183366e0d72c9ca9a8fdc14583f2e42d20978
Anna Bushueva <koxiperu@gmail.com>
2023-10-04 17:05:52 +0200

    names of exercises changed

--- my/Back End/evaluation preparation/app/connection.php
+++ my/Back End/evaluation preparation/app/connection.php
@@ -4,7 +4,7 @@ abstract class Connect
     protected $pdo;
     function __construct()
     {
-        $this->pdo = new PDO('mysql:host=db;dbname=users','root','root');
+        $this->pdo = new PDO('mysql:host=db1;dbname=users','root','root');
     }
 }
 ?>
